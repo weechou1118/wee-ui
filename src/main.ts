1ac52f2f0d55ff05d82b3ba033059a26bc02f77e
zhouwei <361772818@qq.com>
2023-02-07 21:30:19 +0800

    feat: 页面初始化，实现初步的路由跳转

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,21 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '@/components/home.vue'
+import Doc from '@/components/doc.vue'
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', component: Home },
+  { path: '/doc', component: Doc }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+const app = createApp(App)
+app.use(router)
+
+app.mount('#app')
