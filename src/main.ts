58352675e1365a5401b74eaf4230cc5709f12ae3
zhouwei <361772818@qq.com>
2023-02-07 21:55:33 +0800

    feat: 删除脚手架生成的默认样式

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '@/components/home.vue'
 import Doc from '@/components/doc.vue'
+import './assets/normalize.css'
 
 const routes = [
   { path: '/', component: Home },
