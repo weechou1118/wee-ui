2752b37789e180de7cb01df060d9d9ec7af697a2
zhouwei <361772818@qq.com>
2023-02-08 09:11:46 +0800

    feat: 调整目录架构

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import Router from './router'
-import './assets/normalize.css'
+import './assets/css/normalize.css'
 
 const app = createApp(App)
 app.use(Router)
