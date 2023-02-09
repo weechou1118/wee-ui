54286ded521d36f65140abd5ddc15473a09077de
zhouwei <361772818@qq.com>
2023-02-09 10:15:56 +0800

    feat: 新增Markdown组件，将md文件包装成组件塞到路由配置中

---
+++ src/components/Markdown.vue
@@ -0,0 +1,12 @@
+<template>
+  <article class="markdown" v-html="content"></article>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  content: {
+    type: String,
+    default: ''
+  }
+})
+</script>
