d52c5d3f574c13908995d887dfed3fdce070267f
zhouwei <361772818@qq.com>
2023-02-12 12:55:47 +0800

    feat: 添加代码展示开关

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -1,24 +1,48 @@
 <template>
   <div class="preview-container">
-    <h2>{{ el!.__sourceCodeTitle }}</h2>
+    <h2 class="preview-container-title">
+      {{ el!.__sourceCodeTitle }}
+      <Button @click="checkMode = !checkMode">
+        {{ checkMode ? '查看代码' : '隐藏代码'  }}
+      </Button>
+    </h2>
     <div class="preview-container-component">
       <component :is="el"></component>
     </div>
-    <div class="preview-container-code">
+    <div v-if="!checkMode" class="preview-container-code">
       <pre class="language-js">{{ el!.__sourceCode }}</pre>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+import Button from '@/lib/button/index.vue'
 const props = defineProps({
   el: {
     type: Object,
     require: true
   }
 })
+const checkMode = ref(false)
 </script>
 
 <style scoped lang="scss">
-  
+.preview-container {
+  min-width: 400px;
+  &-title {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid #eaeaea;
+    padding: 10px 20px;
+  }
+  &-component {
+    padding: 10px 20px;
+  }
+  &-code {
+    border-top: 1px dashed #eaeaea;
+    padding: 10px 20px;
+  }
+}
 </style>
