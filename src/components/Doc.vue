64ea0ea89ecf8f6a10d0260b7a1884c226a6833a
zhouwei <weech0u@foxmail.com>
2023-02-17 16:22:50 +0800

    feat: 封装参数组件

---
+++ src/components/Doc.vue
@@ -0,0 +1,39 @@
+<template>
+  <table>
+    <tr>
+      <th v-for="(item, index) in headerList" :key="index">{{ item }}</th>
+    </tr>
+    <tr v-for="(trItem, index) in content" :key="index">
+      <td v-for="(tdItem, index) in trItem" :key="index">{{ tdItem }}</td>
+    </tr>
+  </table>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const props = defineProps({
+  content: {
+    type: Array,
+    default: () => []
+  }
+})
+
+const { content } = props
+const headerList = ref(['参数', '描述', '类型', '可选值', '默认值'])
+</script>
+
+<style setup lang="scss">
+table {
+  width: 80%;
+  margin-top: 20px;
+  tr {
+    th {
+      text-align: left;
+    }
+    th, td {
+      border-bottom: 1px solid #dcdfe6;
+      height: 45px;
+    }
+  } 
+}
+</style>
