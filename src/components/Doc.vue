64257490a0b9d242c3e9aabfb7a997ec20349a5c
zhouwei <weech0u@foxmail.com>
2023-02-20 11:14:17 +0800

    feat: Doc组件小重构

--- src/components/Doc.vue
+++ src/components/Doc.vue
@@ -1,7 +1,7 @@
 <template>
   <table>
     <tr>
-      <th v-for="(item, index) in headerList" :key="index">{{ item }}</th>
+      <th v-for="(item, index) in headerProps" :key="index">{{ item }}</th>
     </tr>
     <tr v-for="(trItem, index) in content" :key="index">
       <td v-for="(tdItem, index) in trItem" :key="index">{{ tdItem }}</td>
@@ -10,22 +10,37 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed } from 'vue'
 const props = defineProps({
+  type: {
+    type: String,
+    values: ['attr', 'slot', 'event'],
+    default: 'attr'
+  },
   content: {
     type: Array,
-    default: () => []
+    default: () => ([])
   }
 })
 
-const { content } = props
-const headerList = ref(['参数', '描述', '类型', '可选值', '默认值'])
+const { type, content } = props
+const headerProps = computed(() => {
+  switch (type) {
+    case 'attr':
+      return ['参数', '描述', '类型', '可选值', '默认值']
+    case 'slot':
+      return ['插槽名', '描述']
+    case 'event':
+      return ['事件名', '描述', '回调参数']
+  }
+})
 </script>
 
 <style setup lang="scss">
 table {
   width: 80%;
   margin-top: 20px;
+  margin-bottom: 30px;
   tr {
     th {
       text-align: left;
