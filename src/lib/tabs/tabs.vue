2ac67b0542e7c7fdb43fa80b775a0dee7a34b0d8
zhouwei <weech0u@foxmail.com>
2023-03-08 10:53:47 +0800

    feat: tabs组件逻辑优化

--- src/lib/tabs/tabs.vue
+++ src/lib/tabs/tabs.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="wee-tabs" :class="classes">
-    <slot></slot>
+  <div class="wee-tabs" :class="classes" ref="container">
+    <span v-for="label in labels" :key="label" class="wee-tab" :class="{ 'is-active': modelValue === label }"
+      @click="handleClick(label)" :ref="getItemRef(label)">
+      {{ label }}
+    </span>
+    <div v-if="type === 'line'" class="wee-tabs-indicator" ref="indicator"></div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { toRefs, provide, computed } from 'vue'
-const emits = defineEmits(['tabClick', 'update:modelValue'])
+import { computed, useSlots, ref, onMounted, watchEffect } from 'vue'
+const emits = defineEmits(['update:modelValue', 'changeTab'])
 const props = defineProps({
   modelValue: {
     type: String,
@@ -14,19 +18,40 @@ const props = defineProps({
   },
   type: {
     type: String,
-    default: ''
+    default: 'line'
   }
 })
 
-const changeEvent = (val) => {
-  emits('update:modelValue', val)
-  emits('tabClick')
+const slots = useSlots().default()
+
+const selectedItem = ref<HTMLElement>(null)
+const container = ref<HTMLElement>(null)
+const indicator = ref<HTMLElement>(null)
+
+const labels = slots.map(item => item.props.label)
+// const current = computed(() => {
+//   return slots.find((tab) => tab.props.label === props.modelValue)
+// })
+
+onMounted(() => {
+  watchEffect(() => {
+    renderIndicator()
+  })
+})
+
+const renderIndicator = () => {
+  if (props.type !== 'line') return
+  const { left: left1, width } = selectedItem.value.getBoundingClientRect()
+  const { left: left2 } = container.value.getBoundingClientRect()
+  indicator.value.style.width = width + 'px'
+  indicator.value.style.left = (left1 - left2) + 'px'
 }
 
-provide('WeeTabsKey', {
-  ...toRefs(props),
-  changeEvent
-})
+const getItemRef = (label) => {
+  return (ref) => {
+    if (label === props.modelValue) selectedItem.value = ref
+  }
+}
 
 const classes = computed(() => {
   return {
@@ -34,16 +59,58 @@ const classes = computed(() => {
   }
 })
 
+const handleClick = (val) => {
+  emits('update:modelValue', val)
+  emits('changeTab')
+}
+
 </script>
 
 <style scoped lang="scss">
 .wee-tabs {
   display: flex;
   border-bottom: 1px solid #eaeaea;
+  position: relative;
+
+  &-indicator {
+    position: absolute;
+    bottom: 0;
+    height: 2px;
+    background-color: #409eff;
+    transition: all .2s ease;
+  }
+
+  .wee-tab {
+    display: inline-block;
+    margin-right: 20px;
+    padding-bottom: 4px;
+    cursor: pointer;
+    background-color: transparent;
+    transition: background-color .3s ease;
+
+    &.is-active {
+      color: #409eff;
+      // border-bottom: 2px solid #409eff;
+    }
+  }
+
   &-card {
     background-color: #f7f7fa;
     padding: 10px;
     border-bottom: none;
+
+    .wee-tab {
+      border-bottom: none;
+      color: #000;
+      padding: 10px 20px;
+      border-radius: 10px;
+      flex: 1;
+      text-align: center;
+
+      &.is-active {
+        background-color: #fff;
+      }
+    }
   }
 }
 </style>
